d40edc89b2911654d1914c1a1b10855faea42465
respeaker <respeaker@seeed.cc>
2019-02-06 20:59:10 +0100

    parallel

---
+++ emitter.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt #import the client1
+import time
+import json
+
+############
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+########################################
+broker_address="192.168.9.170"
+#broker_address="iot.eclipse.org"
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+client.loop_start() #start the loop
+print("Publishing message to topic","sat/volume")
+
+payload = {
+    "volume": 3,
+    "siteId": "pi3sat1"
+}
+
+din=json.dumps(payload)
+print din
+print payload
+client.publish("sat/volume",din)
+client.loop_stop()
---
+++ listener.py
@@ -0,0 +1,41 @@
+import paho.mqtt.client as paho
+import time
+import alsaaudio
+import json
+
+siteId = "pi3sat1"
+MQTT_IP_ADDR = "192.168.9.170"
+MQTT_PORT = 1883
+
+def on_sat_volume(volume):
+    print("volume " + str(volume))
+
+def on_message(client, userdata, message):
+    topic = message.topic
+    msg=str(message.payload.decode("utf-8","ignore"))
+    msgJson=json.loads(msg)
+
+    if "siteId" in msgJson:
+        if msgJson["siteId"] != siteId:
+            return
+
+    print "[Topic]: " + str(topic)
+    print "[Payload]: " + msg
+
+    if topic == "sat/volume":
+        on_sat_volume(volume=msgJson["volume"])
+
+#def on_connect(a,b,c,f):
+#    print("connected")
+
+#def on_log(client, userdata, level, buf):
+#    print("log: ",buf)
+
+
+tmpClient = paho.Client("control1")
+tmpClient.on_message=on_message
+#tmpClient.on_connect=on_connect
+#tmpClient.on_log=on_log
+tmpClient.connect(MQTT_IP_ADDR, MQTT_PORT)
+tmpClient.subscribe("sat/volume")
+tmpClient.loop_forever()
