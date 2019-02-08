e166314c986bedefadd4ff823145ed6bf95902c8
respeaker <respeaker@seeed.cc>
2019-02-08 15:51:32 +0100

    rename

--- emitter.py
+++ emitter.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt
 import time
 import json
 
-############
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
-########################################
-broker_address="192.168.9.170"
-#broker_address="iot.eclipse.org"
-print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-client.loop_start() #start the loop
-print("Publishing message to topic","sat/volume")
 
-payload = {
-    "volume": 3,
-    "siteId": "pi3sat1"
-}
+client = mqtt.Client("sk1")
+client.on_message=on_message
 
-din=json.dumps(payload)
-print din
-print payload
-client.publish("sat/volume",din)
+client.connect("192.168.9.170", "1883")
+client.loop_start()
+
+payload = {"volume": 3, "siteId": "pi3sat1"}
+jsonPayload = json.dumps(payload)
+client.publish("sat/volume", jsonpayload)
 client.loop_stop()
