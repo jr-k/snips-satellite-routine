4902ff00bc445767d9531352194b5fef115d9dc2
respeaker <respeaker@seeed.cc>
2019-02-10 18:19:05 +0100

    dyn

--- listener.py
+++ listener.py
@@ -4,12 +4,39 @@ import paho.mqtt.client as paho
 import time
 import alsaaudio
 import json
+import pytoml
+import os
 
-time.sleep(10)
+#time.sleep(1)
 
-siteId = "pi3sat1"
-MQTT_IP_ADDR = "192.168.9.170"
-MQTT_PORT = 1883
+SNIPS_CONFIG_PATH = '/etc/snips.toml'
+siteId = 'default'
+mqttServer = '127.0.0.1'
+mqttPort = 1883
+
+
+def loadConfigs():
+	global mqttServer, mqttPort, siteId, hotwordId
+
+	if os.path.isfile(SNIPS_CONFIG_PATH):
+		with open(SNIPS_CONFIG_PATH) as confFile:
+			configs = pytoml.load(confFile)
+			if 'mqtt' in configs['snips-common']:
+				if ':' in configs['snips-common']['mqtt']:
+					mqttServer = configs['snips-common']['mqtt'].split(':')[0]
+					mqttPort = int(configs['snips-common']['mqtt'].split(':')[1])
+				elif '@' in configs['snips-common']['mqtt']:
+					mqttServer = configs['snips-common']['mqtt'].split('@')[0]
+					mqttPort = int(configs['snips-common']['mqtt'].split('@')[1])
+			if 'bind' in configs['snips-audio-server']:
+				if ':' in configs['snips-audio-server']['bind']:
+					siteId = configs['snips-audio-server']['bind'].split(':')[0]
+				elif '@' in configs['snips-audio-server']['bind']:
+					siteId = configs['snips-audio-server']['bind'].split('@')[0]
+			if 'hotword_id' in configs['snips-hotword']:
+				hotwordId = configs['snips-hotword']['hotword_id']
+	else:
+		print('Snips configs not found')
 
 def on_sat_volume(volume):
     print("volume " + str(volume))
@@ -52,7 +79,7 @@ def on_message(client, userdata, message):
     print "[Topic]: " + str(topic)
     print "[Payload]: " + msg
 
-    if topic == "sat/volume":
+    if topic == "hermes/volume/set":
         on_sat_volume(volume=msgJson["volume"])
 
 def on_connect(client, userdata, flag, rc):
@@ -65,11 +92,11 @@ def on_disconnected(client, userdata, rc):
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
 
-
+loadConfigs()
 tmpClient = paho.Client("tmp_"+siteId)
 tmpClient.on_message=on_message
 tmpClient.on_connect=on_connect
 tmpClient.on_log=on_log
-tmpClient.connect(MQTT_IP_ADDR, MQTT_PORT)
-tmpClient.subscribe("sat/volume")
+tmpClient.connect(mqttServer, mqttPort)
+tmpClient.subscribe("hermes/volume/set")
 tmpClient.loop_forever()
